ab010496715be8f31cf751666d6292c36ef4a2e6
Sofiro11 <133816198+Sofiro11@users.noreply.github.com>
2025-10-30 10:41:26 -0500

    resuelto

---
+++ Caso3/src/colas/BuzonEntrada.java
@@ -0,0 +1,26 @@
+package colas;
+
+public class BuzonEntrada {
+    int capacidad;
+    int capacidadMaxima;
+    public BuzonEntrada(int capacidadMaxima){
+        this.capacidad=0;
+        this.capacidadMaxima = capacidadMaxima;
+    }
+    
+    public synchronized void recibirCorreo(){
+        while(capacidad== capacidadMaxima){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        capacidad++;
+    }
+
+    public synchronized void entregarCorreo(){
+        capacidad--;
+        notify();
+    }
+}
