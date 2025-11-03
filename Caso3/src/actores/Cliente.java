273065d485d17d4b368604f2bac3b37c6d41953d
Sofiro11 <133816198+Sofiro11@users.noreply.github.com>
2025-11-03 14:19:36 -0500

    merge

--- Caso3/src/actores/Cliente.java
+++ Caso3/src/actores/Cliente.java
@@ -1,4 +1,23 @@
 package actores;
-public class Cliente {
+
+import colas.BuzonEntrada;
+
+public class Clientes extends Thread{
+    private final BuzonEntrada entrada;
+
+    public Clientes(BuzonEntrada entrada){
+        this.entrada = entrada;
+    }
+
+    @Override
+    public void run(){
+        try{
+            entrada.entregarCorreo();
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
     
 }
