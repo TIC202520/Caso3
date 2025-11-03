6dfb02eea62d6486a8ce45698d139806ca7dd138
Sofiro11 <133816198+Sofiro11@users.noreply.github.com>
2025-11-03 16:28:47 -0500

    cuarentena

--- Caso3/src/actores/ManejadorCuarentena.java
+++ Caso3/src/actores/ManejadorCuarentena.java
@@ -1,4 +1,43 @@
 package actores;
-public class ManejadorCuarentena {
-    
+
+import colas.Cuarentena;
+import colas.BuzonEntrega;
+
+import java.util.List;
+
+public class ManejadorCuarentena extends Thread {
+
+    private final Cuarentena cuarentena;
+    private final BuzonEntrega buzonEntrega;
+    private int contadorProcesados = 0;
+
+    public ManejadorCuarentena(Cuarentena cuarentena, BuzonEntrega buzonEntrega) {
+        super("ManejadorCuarentena");
+        this.cuarentena = cuarentena;
+        this.buzonEntrega = buzonEntrega;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(1000); // Paso de 1 segundo
+                List<String> correosListos = cuarentena.tickYExtraerListos();
+
+                for (String correo : correosListos) {
+                    contadorProcesados++;
+                    boolean descartar = (contadorProcesados <= 21) && (contadorProcesados % 7 == 0);
+                    if (!descartar) {
+                        buzonEntrega.guardarCorreo(correo);
+                    }
+                }
+                if (cuarentena.finMarcado() && cuarentena.estaVacia()) {
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
+
--- Caso3/src/colas/Cuarentena.java
+++ Caso3/src/colas/Cuarentena.java
@@ -1,13 +1,67 @@
 package colas;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * Cuarentena basada en una cola.
+ * - Almacena correos SPAM con un TimeToLive en segundos (de 10 a 20).
+ */
 public class Cuarentena {
 
-    public void enviarFin() {
-        // TODO 
+    private static final int MIN_SEG = 10;
+    private static final int MAX_SEG = 20;
+
+    private static final class MensajeCuarentena {
+        final String correo;
+        int Segundos;
+        MensajeCuarentena(String correo, int Segundos) {
+            this.correo = correo;
+            this.Segundos = Segundos;
+        }
     }
 
-    public void guardarCorreo(String correo) {
-        // TODO 
+    private final Queue<MensajeCuarentena> colaCuarentena = new LinkedList<>();
+    private final Random random = new Random();
+    private boolean finMarcado = false;
+
+    public synchronized void guardarCorreo(String correo) {
+        int TimeToLive = MIN_SEG + random.nextInt(MAX_SEG - MIN_SEG + 1);
+        colaCuarentena.add(new MensajeCuarentena(correo, TimeToLive));
     }
-    
+
+    public synchronized void enviarFin() {
+        finMarcado = true;
+        notifyAll();
+    }
+
+    public synchronized boolean finMarcado() {
+        return finMarcado;
+    }
+
+    public synchronized boolean estaVacia() {
+        return colaCuarentena.isEmpty();
+    }
+
+    public synchronized List<String> tickYExtraerListos() {
+        for (MensajeCuarentena item : colaCuarentena) {
+            item.Segundos -= 1;
+        }
+
+        List<String> correosListos = new ArrayList<>();
+        List<MensajeCuarentena> aEliminar = new ArrayList<>();
+        for (MensajeCuarentena item : colaCuarentena) {
+            if (item.Segundos <= 0) {
+                correosListos.add(item.correo);
+                aEliminar.add(item);
+            }
+        }
+        colaCuarentena.removeAll(aEliminar);
+
+        return correosListos;
+    }
 }
+
